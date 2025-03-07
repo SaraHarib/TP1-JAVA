ddf16cddddc1822f77360e0e3cd9bdd4d8b94484
SaraHarib <saraharib12@gmail.com>
2025-03-07 21:47:34 +0000

    Update Exercice4.java

--- src/ma/project/exercices/Exercice4.java
+++ src/ma/project/exercices/Exercice4.java
@@ -1,52 +1,51 @@
 package ma.project.exercices;
 
-import java.util.Scanner;
-
 public class Exercice4 {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Saisir le nombre de lignes :  ");
-		int L = sc.nextInt();
-
-		System.out.print("Saisir le nombre de colonnes : ");
-		int C = sc.nextInt();
-
-		int tab[][] = new int[L][C];
-
-		for (int i = 0; i < L; i++) {
-			for (int j = 0; j < C; j++) {
-				System.out.print("Saisir le nombre de la ligne " + (i + 1) + " et la colonne " + (j + 1) + " : ");
-				int n = sc.nextInt();
-				tab[i][j] = n;
-			}
-		}
-
-		System.out.println();
-		for (int i = 0; i < L; i++) {
-			for (int j = 0; j < C; j++) {
-				System.out.print("  " + tab[i][j]);
-			}
-			System.out.println();
-		}
-		int s[] = new int[L];
-		for (int i = 0; i < L; i++) {
-			for (int j = 0; j < C; j++) {
-				s[i] += tab[i][j];
-			}
-		}
-		int m = 0; 
-		for (int i = 1; i < L; i++) {
-		    if (s[i] > s[m]) {
-		        m = i;
-		    }
-		}
-		System.out.println();
-		for (int i = 0; i < C; i++) {
-			System.out.print(" " + tab[m][i]);
-		}
-		System.out.println(" = " + s[m]);
-	}
-
+    
+    public static int kadaneAlgorithm(int[][] T, int n, int m) {
+        int max_t = 0;
+        int pos = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int maxEndingHere = 0;
+            int maxSoFar = 0;
+            
+            for (int j = 0; j < m; j++) {
+                maxEndingHere += T[i][j];
+                maxEndingHere = Math.max(maxEndingHere, 0);
+                maxSoFar = Math.max(maxSoFar, maxEndingHere);
+            }
+            
+            if (maxSoFar > max_t) {  
+                max_t = maxSoFar;
+                pos = i;  
+            }
+        }
+        
+        System.out.print("Sous tableau maximal [");
+        for (int j = 0; j < m; j++) {
+            System.out.print("\t" + T[pos][j]);
+        }
+        System.out.println("]");
+        
+        return max_t;
+    }
+    
+    public static void main(String[] args) {
+        int[][] T = {
+            {-2, 1, -3, 4},
+            {-1, 2, 1, -5},
+            {4, -1, 2, 1}
+        };
+        
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                System.out.print(T[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        int k = kadaneAlgorithm(T, T.length, T[0].length);
+        System.out.println("\tSomme est : " + k);
+    }
 }
